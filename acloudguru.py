0261683cc218619b16ca153837e2bf72d8ab8f2e
Code Mountains <thecodemountains@gmail.com>
2023-11-26 11:29:04 -0600

    part 2 complete - generate and store acloudguru aws credetnials

--- acloudguru.py
+++ acloudguru.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import time
 
 # Initialize the WebDriver (Using Chrome in this example)
@@ -39,29 +40,29 @@ try:
     print("Login successful")
 except TimeoutException:
     print("Login failed or timeout reached")
-
+    browser.quit()
+    exit(1)
 
 # Wait for the page to load
 time.sleep(5)
 
-
 # _____________________________________________________________________________
-
 
 # PART 2 Start AWS Sandbox
 
-
-# Locate and click the 'Start AWS Sandbox' button
-# Here, we use the 'data-cy' attribute to find the button
-start_sandbox_button = WebDriverWait(browser, 10).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, "button[data-cy='button--start-sandbox']"))
-)
-start_sandbox_button.click()
-
+# Increase the timeout period for waiting for the 'Start AWS Sandbox' button
+try:
+    start_sandbox_button = WebDriverWait(browser, 20).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "button[data-cy='button--start-sandbox']"))
+    )
+    start_sandbox_button.click()
+except TimeoutException:
+    print("Failed to find the 'Start AWS Sandbox' button within the given time.")
+    browser.quit()
+    exit(1)
 
 # Wait for the page to load
 time.sleep(3)
-
 
 # Locate the input elements for the username and password
 credential_elements = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "input[aria-label='Copy to clipboard']")))
@@ -88,7 +89,6 @@ with open('credentials.txt', 'w') as file:
     file.write(f"URL: {url}\n")
     file.write(f"access_key_id: {access_key_id}\n")
     file.write(f"secret_access_key: {secret_access_key}\n")
-
 
 # Close the browser after completing tasks
 browser.quit()
